94afb46df3603a9619085f2a6386ad7099143a3b
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-07-22 20:47:37 -0300

    add input html and js

--- Javascript/Organizing-Code/script.js
+++ Javascript/Organizing-Code/script.js
@@ -1,4 +1,4 @@
-
+// books
 function book(title, author, pages, state){
     this.title = title;
     this.author = author;
@@ -10,6 +10,39 @@ function book(title, author, pages, state){
     }
 }
 
+let myLibrary = [];
+
+function addBookToLibrary(title, author, pages, state){
+    let newBook = new book(title, author, pages, state);
+    myLibrary.push(newBook);
+
+}
+
+function displayBooks(){
+    for (let book in myLibrary) {
+        
+    }
+}
+
+// input
+const inputTitle = document.getElementById('title');
+const inputAuthor = document.getElementById('author');
+const inputPages = document.getElementById('pages');
+const inputState = document.getElementById('state');
+const submitBtn = document.getElementById('submitBtn');
+
+// Main
+submitBtn.addEventListener('click', ()=>{
+    addBookToLibrary(inputTitle.value, inputAuthor.value, inputPages.value, inputState.value);
+    inputTitle.value = '';
+    inputAuthor.value = '';
+    inputPages.value = '';
+    inputState.value = '';
+
+
+});
+
+
+
 // let book1 = new book('Road to Azaroth', 'Ravin Enger', 300, 'finished');
 // console.log(book1.info());
-
