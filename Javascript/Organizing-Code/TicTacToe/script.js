9d0a2180eeda61b9f07f9da3be8b576aa37365ec
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-07-30 02:37:09 -0300

    add gameboard reset funciton and comments

--- Javascript/Organizing-Code/TicTacToe/script.js
+++ Javascript/Organizing-Code/TicTacToe/script.js
@@ -1,11 +1,11 @@
 
-function logger(){
-    console.log('test')
-}
-
 // Gameboard Object
 const gameboard = (
     () =>{
+        // Get reference to DOM Element
+        let boardDOM = document.getElementsByClassName('board')[0].children;
+        
+        // Create Map Array of cells state in board
         let boardArray = 
         [
             false, false, false,
@@ -13,30 +13,28 @@ const gameboard = (
             false, false, false
         ]; 
 
-        // function to mark cell in board
-        // when a user clicks in the cell, the array must 
-        // tick the respective cell as 1 or -1 depending
-        // on wich player moved.
-        
-        let boardDOM = document.getElementsByClassName('board')[0].children;
+        // Variable to keep track of current player
         let currentPlayer = 1;
         
+        // Loop through DOM cells and add click event listener 
         for (let index = 0; index < boardDOM.length; index++) {
             let element = boardDOM[index];
-            element.addEventListener('click', ()=>{
+            
+            element.addEventListener('click', ()=>{    
                 if(boardArray[index] === false){
+                    
+                    // mark in boardArray current player id
                     boardArray[index] = currentPlayer;
+                    
                     if(currentPlayer === 1){
                         boardDOM[index].classList.add(`player${currentPlayer}`);
                         currentPlayer = 2                        
-                        boardDOM[index].classList.remove(`player${currentPlayer}`);
                     }
                     else{
                         boardDOM[index].classList.add(`player${currentPlayer}`);
                         currentPlayer = 1
-                        boardDOM[index].classList.remove(`player${currentPlayer}`);
                     }
-                    console.log(gameboard.boardArray)
+
                 }
 
             });
@@ -49,9 +47,13 @@ const gameboard = (
         };
 
         // function to reset board
-        // all cells must go back to false
+        function reset(){
+            for (let index = 0; index < boardArray.length; index++) {
+                boardArray[index] = false;
+                boardDOM[index].classList.remove('player1', 'player2')
+            }
+        };
 
-
-        return{boardArray, getCurrent}
+        return{getCurrent, reset};
     }
 )();
