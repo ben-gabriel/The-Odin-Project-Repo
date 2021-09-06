ed941f40e4dadf3daae4e2ef2453509783fac9e8
Sergio Gabriel Benitez <ben.gabriel.w@gmail.com>
2021-09-06 15:48:22 -0300

    add winter and summer files

--- Node/w3schools/app.js
+++ Node/w3schools/app.js
@@ -1,20 +1,26 @@
 const fs = require('fs');
 const http = require('http');
-
-
-let myHtml;
-fs.readFile('./index.html', 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    console.log(data)
-    myHtml = data;
-  })
-
-
+const url = require('url');
 
 http.createServer((req, res)=>{
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(myHtml);
+    
+    let reqPath = req.url;
+    console.log('requestes path ==== '+reqPath);
+    if(reqPath == '/'){
+        reqPath = '/index'
+    }
+
+    fs.readFile('.'+reqPath+'.html', function(err, data) {
+        if (err) {
+            console.log(err)
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        } 
+        
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+
 }).listen(8080);
