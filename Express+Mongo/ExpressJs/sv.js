ba2ad7ae86faa11370cf31611de4186a382260d9
Sergio <ben.gabriel.w@gmail.com>
2021-10-28 15:01:08 -0300

    add practice express syntax

--- Express+Mongo/ExpressJs/sv.js
+++ Express+Mongo/ExpressJs/sv.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express();
 const port = 2404;
 
+const fs = require('fs');
+const wdir = './ExpressJs';
+
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req, res)=>{    
+
+    fs.readFile(wdir+'/index.html', function(error, content){
+        res.set('Content-Type', 'text/html')
+        res.send(content)
+    });
+
+});
+
+app.post('/', (req, res)=>{    
+    console.log(req.body);
+
+    res.redirect('/')
+
+});
+
 
 app.listen(port);
 console.log(`Server Listening in Port ${port}`);
