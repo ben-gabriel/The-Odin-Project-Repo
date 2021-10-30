9119293d0cdded3b9f446fa548b1fa384df8f704
Sergio <ben.gabriel.w@gmail.com>
2021-10-30 18:48:23 -0300

    add first ejs syntax

--- Express+Mongo/ExpressJs/sv.js
+++ Express+Mongo/ExpressJs/sv.js
@@ -2,26 +2,32 @@ const express = require('express');
 const app = express();
 const port = 2404;
 
-const fs = require('fs');
-const { nextTick } = require('process');
+const path = require('path');
 const wdir = './ExpressJs'; // working directory
 
+const fs = require('fs');
 
-app.use(express.urlencoded({extended:true})); // middleware to parse body
+const ejs = require('ejs');
+
+// -------- Middleware
+app.use(express.urlencoded({extended:true})); // parse body
+
 app.use((req, res, next)=>{
-    console.log(req.originalUrl);
+    console.log(req.originalUrl); // Logger
     next()   
 });
 
+// -------- Settings
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
+// -------- Routes
 app.get('/',(req, res)=>{    
 
-    fs.readFile(wdir+'/index.html', function(error, content){
-        res.set('Content-Type', 'text/html')
-        res.send(content)
-    });
+    res.render('index', {postTitle: 'My very own post title'})
 
 });
+
 app.get('/style.css',(req, res)=>{    
 
     fs.readFile(wdir+'/style.css', function(error, content){
