ef89949c12f3bdae77e2edf28e06cc9bacb5b6bc
Sergio <ben.gabriel.w@gmail.com>
2021-10-30 19:48:02 -0300

    add array data

--- Express+Mongo/ExpressJs/sv.js
+++ Express+Mongo/ExpressJs/sv.js
@@ -22,16 +22,24 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 // -------- Routes
-const postData = {
-    postTitle: 'I Dont Like Sand',
-    postAuthor: 'Anakin',
-    postDate: '20/05/2003',
-    postContent: "I don't like sand. It's coarse and rough and irritating and it gets everywhere."
-}
-
+const postData = [
+    {
+        postTitle: 'I Dont Like Sand',
+        postAuthor: 'Anakin',
+        postDate: '20/05/2003',
+        postContent: "I don't like sand. It's coarse and rough and irritating and it gets everywhere."
+    },
+    {
+        postTitle: `I'm NOT The Father`,
+        postAuthor: 'Darth-Vader',
+        postDate: '05/08/1976',
+        postContent: "Luke, I'm not your father."
+    },
+    
+]
 app.get('/',(req, res)=>{    
 
-    res.render('index', postData);
+    res.render('index', {postData});
 
 });
 
@@ -54,3 +62,9 @@ app.post('/', (req, res)=>{
 
 app.listen(port);
 console.log(`Server Listening in Port ${port}`);
+
+for (const post in postData) {
+    if (postData.length>0) {
+        console.log(postData[post]);
+    }
+}
