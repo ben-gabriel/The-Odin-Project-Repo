6c96810bb32db6206ef5f1ce7c1fbdd2a756af14
Sergio <ben.gabriel.w@gmail.com>
2021-11-01 13:42:36 -0300

    add datbase read and post

--- Express+Mongo/ExpressJs/sv.js
+++ Express+Mongo/ExpressJs/sv.js
@@ -4,10 +4,12 @@ const port = 2404;
 
 const path = require('path');
 const wdir = './ExpressJs'; // working directory
-
 const fs = require('fs');
 
 const ejs = require('ejs');
+
+
+const {MongoClient} = require('mongodb');
 
 // -------- Middleware
 app.use(express.urlencoded({extended:true})); // parse body
@@ -22,24 +24,15 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 // -------- Routes
-const postData = [
-    {
-        postTitle: 'I Dont Like Sand',
-        postAuthor: 'Anakin',
-        postDate: '20/05/2003',
-        postContent: "I don't like sand. It's coarse and rough and irritating and it gets everywhere."
-    },
-    {
-        postTitle: `I'm NOT The Father`,
-        postAuthor: 'Darth-Vader',
-        postDate: '05/08/1976',
-        postContent: "Luke, I'm not your father."
-    },
-    
-]
 app.get('/',(req, res)=>{    
+    console.log('log in / ', database({},1).then((postData)=>{
 
-    res.render('index', {postData});
+            console.log('test in get / ', postData);
+            res.render('index', {postData});
+
+        })
+        
+    );
 
 });
 
@@ -55,7 +48,7 @@ app.get('/style.css',(req, res)=>{
 app.post('/', (req, res)=>{    
     console.log(req.body);
 
-    postData.push(req.body)
+    database(req.body).catch(console.error);
 
     res.redirect('/')
 
@@ -65,8 +58,48 @@ app.post('/', (req, res)=>{
 app.listen(port);
 console.log(`Server Listening in Port ${port}`);
 
-for (const post in postData) {
-    if (postData.length>0) {
-        console.log(postData[post]);
+
+//Database
+async function database(insert, flag=0){
+
+    const uri = 'mongodb://localhost:27017';
+
+    const client = new MongoClient(uri);
+
+    const db = 'quotesBlog';
+    const collection = 'posts';
+
+    try {
+        await client.connect();
+
+        //Crud Operations
+        if(flag==0){
+            await createOneDocument(client, db, collection, insert);
+        }
+        else{
+            const result = await findManyDocuments(client, db, collection);
+            console.log('log inside database(): ', result)
+            return result
+        }
+
+    }catch (e){
+        console.error(e);
+    }finally{
+        await client.close();
     }
+
+}
+
+async function createOneDocument(client, database, collection, newDocument){
+    const result = await client.db(database).collection(collection).insertOne(newDocument);
+
+    console.log(`New Document Created, _ID: ${result.insertedId}`);
+}
+
+async function findManyDocuments(client, database, collection, limit=100){
+    const cursor = await client.db(database).collection(collection).find().limit(limit).sort({name:1});
+    
+    const result = await cursor.toArray();
+    console.log('log inside findMany' , result);
+    return result
 }
