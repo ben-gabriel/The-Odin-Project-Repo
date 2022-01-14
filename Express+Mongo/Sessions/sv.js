4e7eaab2564dc8a9aea7e39b51804a3c5f957067
Sergio <ben.gabriel.w@gmail.com>
2022-01-14 13:31:26 -0300

    add basic files and syntax

--- Express+Mongo/Sessions/sv.js
+++ Express+Mongo/Sessions/sv.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
 
-// Views Engine
+// -------- Settings
 app.set('view engine', 'ejs');
-app.set('views', './Sessions/views')
+app.set('views', './Sessions/views');
 
+app.use(express.urlencoded({extended: false}));
 
-// Routes
+// -------- Routes
 app.get('/', (req, res)=>{
     res.render('index.ejs');
 });
 
+app.get('/login', (req, res)=>{
+    res.render('login.ejs');
+});
+
+app.get('/register', (req, res)=>{
+    res.render('register.ejs');
+});
+app.post('/register', (req, res)=>{
+
+});
+
+
+// -------- Port
 app.listen(2404);
 
+// -------- -------- -------- --------
+// -------- Database Emulator --------
+let users = [];
