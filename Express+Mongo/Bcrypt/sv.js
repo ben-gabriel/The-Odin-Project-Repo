69a0cb60bf372141e7036d587af1e22be2967f21
Sergio <ben.gabriel.w@gmail.com>
2022-01-19 17:29:54 -0300

    add basic bcrypt syntax

--- Express+Mongo/Bcrypt/sv.js
+++ Express+Mongo/Bcrypt/sv.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 2404;
+const bcrypt = require('bcrypt');
 
 app.get('/', (req, res)=>{
     res.send('Hello Bcrypt');
 });
 
-// -------- Post
-app.post('/createUser', (req, res)=>{
-    
-});
-app.post('/Login', (req, res)=>{
+const users = [
+    {name: 'jhon', password: 'jhonpw', hashedPw: ''},
+    {name: 'kyle', password: 'jhonpw', hashedPw: ''},
+    {name: 'jess', password: 'jesspw', hashedPw: ''}
+];
 
+console.log('users = ', users);
+
+const saltPromise = bcrypt.genSalt();
+saltPromise.then((salt)=>{
+    console.log(salt);
+    let hashedPwPromise = bcrypt.hash(users[0].password, salt);
+    hashedPwPromise.then((hashPw)=>{
+        console.log(hashPw)
+    });
 });
+
 
 app.listen(port);
 console.log('Bcrypt Server Listening in port: ',port);
-
-// -------- DataBase
-const {MongoClient} = require('mongodb');
-const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri);
-const db = 'quotesBlog';
-const collection = 'users';
 
