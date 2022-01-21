eb8d7b24a7e3425cf2530918629e3317c21df48e
Sergio <ben.gabriel.w@gmail.com>
2022-01-21 12:48:48 -0300

    add decryption function

--- Express+Mongo/Bcrypt/sv.js
+++ Express+Mongo/Bcrypt/sv.js
@@ -3,11 +3,15 @@ const app = express();
 const port = 2404;
 const bcrypt = require('bcrypt');
 
+// -- Sv + Port
 app.get('/', (req, res)=>{
     res.send('Hello Bcrypt!');
 });
+app.listen(port);
+console.log('Bcrypt Server Listening in port: ', port);
 
-// --------- Database Emulator
+
+// -- Database Emulator
 const users = [
     // password field wouldn't be saved to db, only hashedPw would.
     {name: 'jhon', password: 'jhonpw', hashedPw: ''},
@@ -16,7 +20,8 @@ const users = [
 ];
 console.log('users = ', users); 
 
-// -------- Encrypting password
+
+// -- Encrypting password
 // Takes 1 user object and hashes its 'password' field.
 async function encryptPassword(userObj){
     // generate salt, to mask real password. 
@@ -29,18 +34,36 @@ async function encryptPassword(userObj){
     userObj.hashedPw = hashedPassword;
 }
 
-// Simulate encryption process and log data.
+
+// -- Decrypting password
+// Takes the password to compare against the password in database;
+async function decryptPassword(passwordToCompare, userObj){
+    // compare data and save result. 
+    let comparisonResult = await bcrypt.compare(passwordToCompare, userObj.hashedPw);
+    
+    // admit or deny access to user. 
+    if (comparisonResult === true){
+        console.log(`Comparison SUCCESSFUL - ${passwordToCompare} is the correct password`);
+    }
+    else{
+        console.log(`Comparison FAILED - ${passwordToCompare} is NOT the correct password`);
+    }
+}
+
+
+// -- Simulate encryption process and log data.
 (async function(){
-    await encryptPassword(users[1])
-    await encryptPassword(users[0])
-    await encryptPassword(users[2])
+    await encryptPassword(users[0]);
+    await encryptPassword(users[1]);
+    await encryptPassword(users[2]);
     console.log('users = ', users);
+ 
+    console.log('\njess -> jesspw');
+    await decryptPassword('jesspw', users[2]); // Using the correct password
+    console.log('\njess -> secret');
+    await decryptPassword('secret', users[2]); // Using an incorrect password
+    
 })()
-
-// --------- Sv Port
-app.listen(port);
-console.log('Bcrypt Server Listening in port: ', port);
-
 
 
 
