7e403a212f41b7fd5d5eaab64c53902951b748ed
Sergio <ben.gabriel.w@gmail.com>
2022-01-21 11:52:08 -0300

    add comments

--- Express+Mongo/Bcrypt/sv.js
+++ Express+Mongo/Bcrypt/sv.js
@@ -4,32 +4,40 @@ const port = 2404;
 const bcrypt = require('bcrypt');
 
 app.get('/', (req, res)=>{
-    res.send('Hello Bcrypt');
+    res.send('Hello Bcrypt!');
 });
 
+// --------- Database Emulator
 const users = [
+    // password field wouldn't be saved to db, only hashedPw would.
     {name: 'jhon', password: 'jhonpw', hashedPw: ''},
     {name: 'kyle', password: 'jhonpw', hashedPw: ''},
     {name: 'jess', password: 'jesspw', hashedPw: ''}
 ];
+console.log('users = ', users); 
 
-console.log('users = ', users);
-
-async function createPassword(userObj){
+// -------- Encrypting password
+// Takes 1 user object and hashes its 'password' field.
+async function encryptPassword(userObj){
+    // generate salt, to mask real password. 
     let salt = await bcrypt.genSalt();
+
+    // hash real password.
     let hashedPassword = await bcrypt.hash(userObj.password, salt);
+    
+    // save hashed password to database. 
     userObj.hashedPw = hashedPassword;
 }
 
+// Simulate encryption process and log data.
 (async function(){
-    await createPassword(users[0])
-    await createPassword(users[1])
-    await createPassword(users[2])
+    await encryptPassword(users[1])
+    await encryptPassword(users[0])
+    await encryptPassword(users[2])
     console.log('users = ', users);
 })()
 
-
-
+// --------- Sv Port
 app.listen(port);
 console.log('Bcrypt Server Listening in port: ', port);
 
